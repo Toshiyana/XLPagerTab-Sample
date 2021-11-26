d1b3c9b792889bb9ac13a6ebf376a9e0191dfa75
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-26 14:26:00 +0900

    Initial Commit

---
+++ XLPagerTab-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XLPagerTab-Sample
+//
+//  Created by Toshiyana on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
